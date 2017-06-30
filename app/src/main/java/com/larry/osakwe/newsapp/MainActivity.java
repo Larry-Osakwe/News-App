c7ce88c1c3caf6a76d0a5790e47aa352184bc1f4
Larry <larryosak@gmail.com>
2017-06-29 20:28:14 -0700

    Obtain url to be parsed

--- app/src/main/java/com/larry/osakwe/newsapp/MainActivity.java
+++ app/src/main/java/com/larry/osakwe/newsapp/MainActivity.java
@@ -9,5 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String url = "https://www.giantbomb.com/api/reviews/?api_key=837e532ede8d717222c1247baad46cb354fb9686&format=json&limit=5";
+
     }
 }
